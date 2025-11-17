891c0e8cdf861c8c1a5b191ef37aca0270fcf2dd
Prerana Kapadnis <155218862+cipherxulptor@users.noreply.github.com>
2025-11-17 16:39:47 +0530

    Add files via upload

---
+++ GradeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.devdaljeet.grademanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeManagementSystemApplication.class, args);
+	}
+
+}
