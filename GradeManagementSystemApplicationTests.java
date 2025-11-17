28403689ef4fc565bcf1fd7e3e90eeb36b683ea3
Prerana Kapadnis <155218862+cipherxulptor@users.noreply.github.com>
2025-11-17 16:43:50 +0530

    Add files via upload

---
+++ GradeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdaljeet.grademanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
